48452ba59f0ce3612d3b5cafe4a26565e8084bfa
ferrydjing <ferrydjing@outlook.com>
2020-09-16 13:55:04 +0800

    update: 配置

--- src/js/config.js
+++ src/js/config.js
@@ -1,13 +1,11 @@
-let baseUrl = '';
+let baseUrl = '',
+  appid = 'third',
+  appsecret = 'pre_secret_third';
 if (CUT_APP_ENV === 'build') {
+  appsecret = 'production_secret_third';
   baseUrl = 'https://open-api.hula.vip';
 } else {
   baseUrl = 'https://pre-open-api.hula.vip';
 }
-let appid = 'third',
-  appsecret = 'pre_secret_third';
 
-if (CUT_APP_ENV === 'build') {
-  appsecret = 'production_secret_third';
-}
 export { baseUrl, appid, appsecret };
